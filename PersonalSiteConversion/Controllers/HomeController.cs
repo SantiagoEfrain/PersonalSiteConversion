6fc3da021ac0bdd9db6efa5e98f523c346ddb37e
Efrain Santiago <SantiaEfrain@outlook.com>
2023-05-08 08:41:29 -0500

    Add project files.

---
+++ PersonalSiteConversion/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalSiteConversion.Models;
+using System.Diagnostics;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace PersonalSiteConversion.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Models()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ PersonalSiteConversion/Models/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalSiteConversion.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Name is Required")] //* Field is required
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "* Email is Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Subject { get; set; } = null!;
+
+        [Required(ErrorMessage = "* Message is Required")]
+        [DataType(DataType.MultilineText)] // makes a "textarea" for this field.
+        public string Message { get; set; } = null!;
+    }
+}
